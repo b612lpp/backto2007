8e8dfec4599f4d7d3a05f8c3151c6e8b407a9d94
b612lppW <asoinf@gmail.com>
2020-10-27 13:31:18 +0300

    docker file fixed

--- homework/HW04/docker/setup.sql
+++ homework/HW04/docker/setup.sql
@@ -3,3 +3,4 @@ CREATE TABLE posts(ID int, Title TEXT, Tetxt TEXT);
 INSERT INTO
   posts
 values(1, "Title 1", "Text 1");
+ALTER USER 'root' @'%' IDENTIFIED WITH mysql_native_password BY 'password';
